7e61192ad1cf9106e7b5222c689da207be68b649
Juliandazakipradipta <juliandazaki26@gmail.com>
2024-10-16 15:31:45 +0700

    commit2

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup 
 
-client = MongoClient('mongodb+srv://test:sparta1@project0.wvnom.mongodb.net/')
-db = client.zaki
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
@@ -45,4 +56,4 @@ def movie_get():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=3000, debug=True)
